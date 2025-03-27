e5a228a30a434d2ab7db70a85c51c7b2ed48f2a1
johnno100 <73259783+johnno100@users.noreply.github.com>
2025-03-27 08:22:16 +0000

    still at it not working

--- wellness-app/mobile/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native',
-  rules: {
-    'prettier/prettier': 0,
-    'react-native/no-inline-styles': 0,
-    'react/no-unstable-nested-components': 0,
-  },
-};
